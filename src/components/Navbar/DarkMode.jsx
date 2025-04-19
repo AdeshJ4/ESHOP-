6e7e79466d53a29e434244818a8fbd44eec3bbb2
Adesh Jadhav <adejad1234@gmail.com>
2025-04-19 17:30:17 +0530

    resolve DarkMode changes

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -4,18 +4,20 @@ import DarkButton from '../../assets/website/dark-mode-button.png';
 
 const DarkMode = () => {
 
-    const [theme, setTheme] = useState(() => localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')) : true);    
+    const [theme, setTheme] = useState(() => localStorage.getItem('theme') === 'light');
+
 
     useEffect(() => {
-        localStorage.setItem('theme', JSON.stringify(theme));
-        if(theme){
-            document.documentElement.classList.add("light")
+        localStorage.setItem('theme', theme ? 'light' : 'dark');
+        if (theme) {
+            document.documentElement.classList.add("light");
             document.documentElement.classList.remove("dark");
-        }else{
-            document.documentElement.classList.add("dark")
+        } else {
+            document.documentElement.classList.add("dark");
             document.documentElement.classList.remove("light");
         }
-    });
+    }, [theme]);
+
 
     
     return (
@@ -30,30 +32,3 @@ const DarkMode = () => {
 };
 
 export default DarkMode;
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import LightButton from '../../assets/website/light-mode-button.png';
-// import DarkButton from '../../assets/website/dark-mode-button.png';
-
-// const DarkMode = () => {
-//     const [theme, setTheme ] = useState("light")
-//     return (
-//         <div className='relative'>
-//             <img src={LightButton} alt="" className={`w-12 cursor-pointer absolute right-0 z-10  ${theme === "dark" ? "opacity-0" : "opacity-100"}`} />
-//             <img src={DarkButton} alt="" className={`w-12 cursor-pointer`} />
-//         </div>
-//     )
-// }
-
-
-// export default DarkMode;
